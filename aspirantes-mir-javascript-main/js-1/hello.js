c400b1dc3c6610b18baa8cd48b433d61bd8c5f15
CCortesLeon <126416397+CCortesLeon@users.noreply.github.com>
2023-03-12 01:13:29 -0500

    Add files via upload

---
+++ aspirantes-mir-javascript-main/js-1/hello.js
@@ -0,0 +1,8 @@
+function hola(name) {
+    return "Hola " + name;
+    console.log("Esto nunca se va a imprimir");
+  }
+  
+  console.log(hola("Pedro"));
+  console.log(hola("Juan"));
+  console.log(hola(""));
